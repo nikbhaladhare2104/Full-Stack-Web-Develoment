a236f7cee9a5dc2251c3116443e70f8e906a1473
Nikhil Bhaladhare <nikbhaladhare2104@iitb.ac.in>
2023-10-01 22:57:12 +0530

    Add files via upload

--- 12-Dicee-Game-App/index.js
+++ 12-Dicee-Game-App/index.js
@@ -1,1 +1,24 @@
+var randomNumber1 = Math.floor(Math.random() * 6) +1 
+
+var randomImage1 = "images/dice" + randomNumber1 + ".png";
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImage1);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) +1;
+
+var randomImage2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImage2)
+
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Won.";
+} else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Won.";
+} else{
+    document.querySelector("h1").innerHTML = "Draw.";
+}
 
