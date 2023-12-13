d0f9777e3633f04f30ba54c2e8aa503eb60609d9
Nikhil Bhaladhare <nikbhaladhare2104@gmail.com>
2023-12-13 22:17:58 +0530

    Add files via upload

--- 15-Node.js/3-NPM/index.js
+++ 15-Node.js/3-NPM/index.js
@@ -1,1 +1,25 @@
+// var generateName = require('sillyname');
+// var sillyName = generateName();
+
+
+// If we want to import ranther than "require" a module, we have 
+// to add "type" in package.json file and add "module" to it 
+
+    // "name": "2.3-npm",
+    // "version": "1.0.0",
+    // "description": "I am learning about npm",
+    // "main": "index.js",
+    // "type": "module",
+    // .....
+// Lets import 
+
+import generateName from "sillyname";
+import superheroes from "superheroes";
+
+var sillyName = generateName();
+var superHeroName = superheroes.random();
+
+
+console.log(`My name is ${sillyName} and I am known around the world by ${superHeroName}.`);
+
 
