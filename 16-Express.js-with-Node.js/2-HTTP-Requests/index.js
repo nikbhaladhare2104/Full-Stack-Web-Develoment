7d29f2448ba918a99233b8f19ed9545d795154c7
Nikhil Bhaladhare <nikbhaladhare2104@gmail.com>
2024-01-02 11:23:54 +0530

    Add files via upload

--- 16-Express.js-with-Node.js/2-HTTP-Requests/index.js
+++ 16-Express.js-with-Node.js/2-HTTP-Requests/index.js
@@ -1,1 +1,20 @@
+import express from "express";
 
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Nikhil')
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Me")   // html type is text 
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1> MY Name is Nikhil, I live in India. </h1>")
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
