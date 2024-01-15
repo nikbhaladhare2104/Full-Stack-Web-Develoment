a8ce7befe8aae9899c2f54637fc97b410d151c7d
Nikhil Bhaladhare <nikbhaladhare2104@gmail.com>
2024-01-15 13:27:35 +0530

    Add files via upload

---
+++ 16-Express.js-with-Node.js/4-Middleware/index3.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+function logger(req, res, next){
+  console.log(req.method, "url:", req.url);
+  next();
+}
+app.use(logger);
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
