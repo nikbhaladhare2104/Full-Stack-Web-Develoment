165b7c0c26b80d2b0d610a729801d08cb12ee0b1
Nikhil Bhaladhare <nikbhaladhare2104@gmail.com>
2024-01-20 19:09:48 +0530

    Add files via upload

--- 16-Express.js-with-Node.js/5-Secrets-Project/index.js
+++ 16-Express.js-with-Node.js/5-Secrets-Project/index.js
@@ -1,1 +1,40 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+const app = express();
+const port = 3000;
+var password = "";
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+function checkPassword (req, res, next) {
+    password = req.body["password"];
+    next();
+}
+app.use(checkPassword);
+
+app.post("/check", (req, res) => {
+    if (password === "ILoveProgramming") {
+        res.sendFile(__dirname + "/public/secret.html"); 
+    } else {
+        res.sendFile(__dirname + "/public/index.html");
+        // res.redirect("/");
+    }
+})
+
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+})
 
